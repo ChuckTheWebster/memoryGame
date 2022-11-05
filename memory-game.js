7672774b47141f6ec637df0c61744fcbf7c1ee17
Charlotte Webster <charlotte.shae.webster@gmail.com>
2022-11-04 18:12:15 -0600

    fix image drag bug

--- memory-game.js
+++ memory-game.js
@@ -36,11 +36,16 @@ function createCards(imagesShuffled) {
     divForIndividualCard.className = ('card');
     divForIndividualCard.setAttribute('image-src', image);
 
-    //set card image as front
+    //set card image as the revealed card
     const imgOfGeometry = document.createElement('img');
     imgOfGeometry.src = image;
     imgOfGeometry.id = 'img-' + countForId;
     countForId++;
+    //disables img dragging on webpage (could previously
+    //drag card and see the answer without flipping card)
+    imgOfGeometry.ondragstart = () => {
+      return false;
+    };
 
     //append is better than appendChild
     divForIndividualCard.append(imgOfGeometry);
@@ -49,21 +54,19 @@ function createCards(imagesShuffled) {
 }
 createCards(imagesShuffled);
 
+
 const cards = document.querySelectorAll('.card-container .card');
 console.log(cards);
 let counter = 0;
 let firstCard = '';
 let secondCard = '';
-
+//upon click execute the following:
 cards.forEach((card) => {
   card.addEventListener('click', () => {
-    //const shakingCards = document.querySelectorAll('.card.shake');
+
     const clickedCards = document.querySelectorAll('.card.clicked');
-    if (!clickedCards[1])
+    if (!clickedCards[1]) card.classList.add('clicked');
 
-    card.classList.add('clicked');
-
-    //const clickedCards = document.querySelectorAll('.card.clicked');
     if (counter === 0) {
       firstCard = card;
       counter++;
@@ -71,9 +74,6 @@ cards.forEach((card) => {
       secondCard = card;
       counter = 0;
     }
-
-    //if the THIRD CARD YOU CLICK IS A MATCH TO THE FIRST
-    //BUG
 
     const firstCardSrc = firstCard.getAttribute('image-src');
     let secondCardSrc = '';
@@ -90,20 +90,25 @@ cards.forEach((card) => {
         matchedPairNode[0].classList.remove('clicked');
         matchedPairNode[1].classList.add('correct');
         matchedPairNode[1].classList.remove('clicked');
+        firstCard = '';
+        secondCard = '';
 
 
     } else if (counter === 0 && secondCard) {
-        const incorrectPairNode = document.querySelectorAll('.card.clicked');
-        console.log({incorrectPairNode});
-        incorrectPairNode[0].classList.add('shake');
-        incorrectPairNode[1].classList.add('shake');
+      const incorrectPairNode = document.querySelectorAll('.card.clicked');
+      console.log({incorrectPairNode});
+      incorrectPairNode[0].classList.add('shake');
+      incorrectPairNode[1].classList.add('shake');
 
+      //after being revealed, if incorrect match, shake then
+      //unclick (flip back over) after wait msec
       setTimeout(() => {
         incorrectPairNode[0].classList.remove('shake');
         incorrectPairNode[0].classList.remove('clicked');
         incorrectPairNode[1].classList.remove('shake');
         incorrectPairNode[1].classList.remove('clicked');
         secondCard = '';
+        firstCard = '';
       }, FOUND_MATCH_WAIT_MSECS);
     }
 
